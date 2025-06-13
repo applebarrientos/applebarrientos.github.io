29f15fced81fdb2060d0d058ffc2e32c8b10e77e
Apple Barrientos <apple.barrientos@edu.sait.ca>
2025-06-12 21:31:59 -0600

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/applebarrientos.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/applebarrientos.github.io/' : '',
+
+};
 
 export default nextConfig;
