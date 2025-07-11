be257d65991190e279f9380d05622d29d68ca982
Apple Barrientos <apple.barrientos@edu.sait.ca>
2025-07-10 22:20:55 -0600

    updated build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
+  basePath: isProd ? "/applebarrientos.github.io" : "",
   output: 'export',
   distDir: 'dist',
 };
